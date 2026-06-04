c77d385c0e5933ccb57eb02d75c3fc82d2f7672f
3ljamal <elgamalm527@gmail.com>
2026-06-04 12:55:05 +0300

    Add files via upload

---
+++ const links = document.js
@@ -0,0 +1,27 @@
+const links = document.querySelectorAll("nav a");
+
+links.forEach(link=>{
+    link.addEventListener("click",()=>{
+        links.forEach(l=>l.classList.remove("active"));
+        link.classList.add("active");
+    });
+});
+document.getElementById("contactForm").addEventListener("submit", function(e){
+    e.preventDefault();
+
+    let name = document.getElementById("name").value;
+    let product = document.getElementById("product").value;
+    let message = document.getElementById("message").value;
+
+    let phone = "201555291925";
+
+    let text = `🟢 طلب جديد من الموقع:
+
+👤 الاسم: ${name}
+🪑 المنتج: ${product}
+💬 الاستفسار: ${message}`;
+
+    let url = "https://wa.me/" + phone + "?text=" + encodeURIComponent(text);
+
+    window.location.href = url;
+});;
